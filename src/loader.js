713df6fcf8c07d0906f50f898d010bd8ee9df8cb
Tom Golden <tbjgolden@gmail.com>
2020-09-11 21:12:43 -0500

    back to normality

--- src/loader.js
+++ src/loader.js
@@ -14,8 +14,11 @@ export default function(content) {
     getConfig(configString, defaultGetConfig)
   ));
 
+  console.log("a")
+
   flatten(inputAinsley, wrappedGetConfig)
     .then(flatAinsley => {
+      console.log("b")
       let optsStr = "";
       if (config && config.generate) {
         optsStr = ", {";
@@ -26,6 +29,7 @@ export default function(content) {
         optsStr += "}";
       }
 
+      console.log("c")
       callback(
         null,
         `
